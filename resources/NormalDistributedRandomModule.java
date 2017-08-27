21c0bdb3d21ef90f47d559646e4471e35e2886c2
elbosso <elbosso@tichy.fritz.box>
2017-08-27 07:20:22 +0200

    website update

--- resources/NormalDistributedRandomModule.java
+++ resources/NormalDistributedRandomModule.java
@@ -34,7 +34,7 @@ WENN SIE AUF DIE MOEGLICHKEIT EINES SOLCHEN SCHADENS HINGEWIESEN WORDEN SIND.
 */
 package de.elbosso.dataflowframework.modules.generator.util.generator.generalpurpose;
 
-@javax.annotation.Generated(value="de.elbosso.dataflowframework.processors.GeneratorProcessor", date="2017-08-17T17:10:03.922Z")
+@javax.annotation.Generated(value="de.elbosso.dataflowframework.processors.GeneratorProcessor", date="2017-08-24T17:02:33.375Z")
 public class NormalDistributedRandomModule extends de.netsysit.dataflowframework.modules.BeanContextChildModuleBase
 {
 	static
