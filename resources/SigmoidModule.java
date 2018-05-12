f39e1365cb1cae3e14fa7ec41c5c52f7897e2278
Jürgen Key <sgjfsf@hotmail.com>
2018-05-12 17:17:31 +0200

    website update

--- resources/SigmoidModule.java
+++ resources/SigmoidModule.java
@@ -34,7 +34,7 @@ WENN SIE AUF DIE MOEGLICHKEIT EINES SOLCHEN SCHADENS HINGEWIESEN WORDEN SIND.
 */
 package de.elbosso.dataflowframework.modules.convert.algorithms.functions;
 
-@javax.annotation.Generated(value="de.elbosso.dataflowframework.processors.OneDFunctionProcessor", date="2018-05-03T16:16:04.630Z")
+@javax.annotation.Generated(value="de.elbosso.dataflowframework.processors.OneDFunctionProcessor", date="2018-05-12T07:51:11.29Z")
 public class SigmoidModule extends de.netsysit.dataflowframework.modules.ThreadingBeanContextChildModuleBase
 {
 	static
