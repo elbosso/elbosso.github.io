13fc13e64bc102820b46aff65b10a44b54366fbb
Jürgen Key <sgjfsf@hotmail.com>
2022-07-16 06:14:25 +0200

    website update

--- resources/RectangleInsideRuleModule.java
+++ resources/RectangleInsideRuleModule.java
@@ -34,7 +34,7 @@ WENN SIE AUF DIE MOEGLICHKEIT EINES SOLCHEN SCHADENS HINGEWIESEN WORDEN SIND.
 */
 package de.elbosso.dataflowframework.modules.filter.rules.util.validator.rules;
 
-@javax.annotation.Generated(value="de.elbosso.dataflowframework.processors.ValidatorProcessor", date="2022-07-07T10:46:59.65Z")
+@javax.annotation.Generated(value="de.elbosso.dataflowframework.processors.ValidatorProcessor", date="2022-07-14T18:30:12.867Z")
 public class RectangleInsideRuleModule extends de.netsysit.dataflowframework.modules.ThreadingBeanContextChildModuleBase
 {
 	static
